25b8232cd4ce838f070cb6b8dc084bc2a0a30ddc
newu2012 <newu2011@gmail.com>
2021-05-05 14:26:20 +0500

    Change LogoClicked from LogOut to TicketsScreen

--- serviceDesk_client/src/main/java/team/dna2/serviceDesk_client/controllers/DeveloperTicketsScreenController.java
+++ serviceDesk_client/src/main/java/team/dna2/serviceDesk_client/controllers/DeveloperTicketsScreenController.java
@@ -225,11 +225,11 @@ public class DeveloperTicketsScreenController implements Initializable {
     }
 
     /**
-     * Производит выход из аккаунта, позволяя сменить пользователя.
+     * Переход на экран со списком обращений
      */
     @FXML
-    public void LogOutButtonClicked() {
-        ScreenManager.LogOut(); // Метод временно назначен на логотип
+    public void LogoClicked() {
+        ScreenManager.OpenTickets();
     }
     //endregion
 }
--- serviceDesk_client/src/main/java/team/dna2/serviceDesk_client/controllers/MemberTicketsScreenController.java
+++ serviceDesk_client/src/main/java/team/dna2/serviceDesk_client/controllers/MemberTicketsScreenController.java
@@ -219,11 +219,11 @@ public class MemberTicketsScreenController implements Initializable {
     }
 
     /**
-     * Производит выход из аккаунта, позволяя сменить пользователя.
+     * Переход на экран со списком обращений
      */
     @FXML
-    public void LogOutButtonClicked() {
-        ScreenManager.LogOut(); // Метод временно назначен на логотип
+    public void LogoClicked() {
+        ScreenManager.OpenTickets();
     }
     //endregion
 }
