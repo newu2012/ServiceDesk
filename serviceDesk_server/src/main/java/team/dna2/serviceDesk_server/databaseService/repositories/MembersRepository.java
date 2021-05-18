ed4544494bb19396d1d6c161780a74e1af2367c2
Denis I <38253137+idvlop@users.noreply.github.com>
2021-05-19 02:30:31 +0500

    repositories updated - Changes by newu2012

--- serviceDesk_server/src/main/java/team/dna2/serviceDesk_server/databaseService/repositories/MembersRepository.java
+++ serviceDesk_server/src/main/java/team/dna2/serviceDesk_server/databaseService/repositories/MembersRepository.java
@@ -1,6 +1,9 @@
 package team.dna2.serviceDesk_server.databaseService.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import team.dna2.serviceDesk_server.databaseService.entities.Member;
 import team.dna2.serviceDesk_server.databaseService.entities.Organization;
@@ -8,4 +11,11 @@ import team.dna2.serviceDesk_server.databaseService.entities.User;
 
 @Repository
 public interface MembersRepository extends JpaRepository<Member, Long> {
+    @Query("update Member set isOwner=false where id = :memberId")
+    void updateMemberOwnershipById (Long memberId);
+
+    @Query("update Member set isOwner=true where id=:memberId")
+    void setMemberOwnershipById (Long memberId);
+
+    Long findMemberIdByOrganizationIdAndIsOwnerTrue(Long organizationId);
 }
