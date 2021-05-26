a2a62584046e1793b1c3c915bf5cd15cd2c446aa
newu2012 <newu2011@gmail.com>
2021-05-27 01:59:02 +0500

    Fix previous commit

---
+++ serviceDesk_client/src/main/java/team/dna2/serviceDesk_client/models/TicketCategory.java
@@ -0,0 +1,8 @@
+package team.dna2.serviceDesk_client.models;
+
+import java.util.ArrayList;
+
+public class TicketCategory {
+
+    public static ArrayList<TicketCategory> ticketCategories = new ArrayList<>();
+}
