ae110f57aa34e43b137e423538ec0862bb3e80ce
Denis I <38253137+idvlop@users.noreply.github.com>
2021-05-29 17:32:12 +0500

    authorization configured

--- serviceDesk_server/src/main/java/team/dna2/serviceDesk_server/configurations/Initializer.java
+++ serviceDesk_server/src/main/java/team/dna2/serviceDesk_server/configurations/Initializer.java
@@ -1,6 +1,6 @@
-package team.dna2.serviceDesk_server.configurations;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class Initializer extends AbstractHttpSessionApplicationInitializer {
-}
+//package team.dna2.serviceDesk_server.configurations;
+//
+//import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+//
+//public class Initializer extends AbstractHttpSessionApplicationInitializer {
+//}
---
+++ serviceDesk_server/src/main/java/team/dna2/serviceDesk_server/restControllers/AuthController.java
@@ -0,0 +1,20 @@
+package team.dna2.serviceDesk_server.restControllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.dna2.serviceDesk_server.databaseService.services.UserService;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class AuthController {
+    @Resource
+    UserService userService;
+
+//    @GetMapping("/login")
+//    public void logIn()
+}
