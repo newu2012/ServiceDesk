e8833af6b0bb31a86c8a60dd5c730797d09489fa
Alexander Sakhno <skywalkersakhno@gmail.com>
2021-06-03 11:57:48 +0500

    Доработка DeveloperShowTicketScreen_v2

--- serviceDesk_client/src/main/java/team/dna2/serviceDesk_client/controllers/DeveloperShowTicketScreen_v2Controller.java
+++ serviceDesk_client/src/main/java/team/dna2/serviceDesk_client/controllers/DeveloperShowTicketScreen_v2Controller.java
@@ -1,27 +1,49 @@
 package team.dna2.serviceDesk_client.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import team.dna2.serviceDesk_client.ScreenManager;
 
 public class DeveloperShowTicketScreen_v2Controller {
 
+    @FXML
     public void LogoClicked(MouseEvent mouseEvent) {
     }
 
+    @FXML
     public void MyProfileClicked(MouseEvent mouseEvent) {
     }
 
+    @FXML
     public void MyOrganisationClicked(MouseEvent mouseEvent) {
     }
 
+    @FXML
     public void PreviousScreenButtonClicked(MouseEvent mouseEvent) {
         ScreenManager.TryShowPreviousScreen();
     }
 
+    @FXML
     public void AuthorReopenButtonClicked(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void DeveloperRedTicketButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void TicketsClicked(MouseEvent mouseEvent) {
+        ScreenManager.OpenTickets();
+    }
+
+    @FXML
+    public void CompendiumsClicked(MouseEvent mouseEvent) {
+        ScreenManager.OpenCompendiums();
+    }
+
+    @FXML
+    public void StatisticsClicked(MouseEvent mouseEvent) {
+        ScreenManager.OpenStatistics();
     }
 }
